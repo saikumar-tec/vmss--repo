e557d515356f9bd20afffd1b7cfd202dcd5d1e48
saikumar <saikumarkotrangi668@gmail.com>
2026-03-25 10:04:46 +0530

    terra changes

--- terra.tf
+++ terra.tf
@@ -4,3 +4,9 @@ resource "azurerm_resource_group" "rg"
   name = "demo-rg"
  location = "central india"
 }
+ 
+ resource "azurerm_virtual_network" "vnet"
+  name = demo-vnet
+ location = "azurerm_resource_group.rg.location"
+ resource_group_name = "azurerm_resource_group.rg.location"
+ address_space = ["10.0.0.0/24"]
